86bf01009dcb04e80cf16e822efcb0f85b5aeca8
Rodrigo Bego <rodrigobego@uol.com.br>
2025-08-26 16:37:04 -0300

    scroll suave link interno

--- script.js
+++ script.js
@@ -19,26 +19,44 @@ function initTabNav() {
     });
   }
 }
-initTabNav()
+initTabNav();
 
+function initAccordion() {
+  const accordionList = document.querySelectorAll(".js-accordion dt");
+  const activeClass = "ativo";
 
-function initAccordion(){
-  const accordionList = document.querySelectorAll('.js-accordion dt')
-  const activeClass = 'ativo';
-  
-  if(accordionList.length){
-    accordionList[0].classList.add(activeClass)
-    accordionList[0].nextElementSibling.classList.add(activeClass)
-    
-    function activeAccordion(){
-      this.classList.toggle(activeClass)
-      this.nextElementSibling.classList.toggle(activeClass)
+  if (accordionList.length) {
+    accordionList[0].classList.add(activeClass);
+    accordionList[0].nextElementSibling.classList.add(activeClass);
+
+    function activeAccordion() {
+      this.classList.toggle(activeClass);
+      this.nextElementSibling.classList.toggle(activeClass);
     }
-    
+
     accordionList.forEach((item) => {
-      item.addEventListener('click', activeAccordion)
-    })
+      item.addEventListener("click", activeAccordion);
+    });
   }
 }
+initAccordion();
 
-initAccordion()
+function initScrollSuave() {
+  const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+
+  function scrollToSection(event) {
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute("href");
+    const section = document.querySelector(href);
+
+    section.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+  }
+
+  linksInternos.forEach((link) => {
+    link.addEventListener("click", scrollToSection);
+  });
+}
+initScrollSuave();
