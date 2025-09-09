64a2170806c4ef5afd2bddaedd25b55420baa311
Rodrigo Bego <rodrigobego@uol.com.br>
2025-09-09 16:29:19 -0300

    dropdown menu sobre

---
+++ js/modules/dropdown-menu.js
@@ -0,0 +1,17 @@
+export default function initDropdownMenu(){
+
+}
+
+const dropdownMenus = document.querySelectorAll('[data-dropdown]')
+
+dropdownMenus.forEach(menu => {
+  ['touchStart', 'click'].forEach(userEvent => {
+    menu.addEventListener(userEvent, handleClick)
+  })
+})
+
+function handleClick(event){
+  event.preventDefault();
+  this.classList.toogle('active')
+}
+
